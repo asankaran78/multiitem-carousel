a60a3912cf5d98aff8b6cd16e157f201576735f8
asankaran78 <asankaran78@gmail.com>
2019-01-04 07:05:55 -0600

    Update MultiItemsCarousel.jsx

--- src/MultiItemsCarousel.jsx
+++ src/MultiItemsCarousel.jsx
@@ -3,35 +3,42 @@ import ItemsCarousel from 'react-items-carousel';
 import range from 'lodash/range';
 
 class MultiItemsCarousel extends React.Component {
-
   componentWillMount() {
     this.setState({
       children: [],
       activeItemIndex: 0,
+      navSummary: myJson.navSummary
     });
 
     setTimeout(() => {
       this.setState({
-        children: this.createChildren(15),
-      })
+        children: this.createChildren()
+      });
     }, 100);
   }
 
- /* createChildren = n => range(n).map(i => <div key={i} style={{ height: 87, background: '#EDF0F4' }}>
-        <p>Franklin Group Investors's fund (FCGLX)</p>{i}</div>); */
 
-    createChildren = n => range(n).map(i => <div key={i} className="carouselDisplay">
-        <p>Franklin Group Investors's fund (FCGLX)</p>
-        <span className="carouselInner">Awaiting Approval</span>
-        </div>); 
+  createChildren = () =>{
+    var arr = [];
+    for(let i=0;i<this.state.navSummary.response.length;i++){
+        arr.push(<div
+          key={i + 1}
+          className="carouselDisplay"
+          onClick={this.handleIndexClick}
+        >
+          <p>{this.state.navSummary.response[i].fundName}</p>
+          <p />
+          <span className="carouselInner">{this.state.navSummary.response[i].navStatus}</span>
+        </div>);
+    }
+    return arr;
+  }
 
-  changeActiveItem = (activeItemIndex) => this.setState({ activeItemIndex });
+
+  changeActiveItem = activeItemIndex => this.setState({ activeItemIndex });
 
   render() {
-    const {
-      activeItemIndex,
-      children,
-    } = this.state;
+    const { activeItemIndex, children } = this.state;
 
     return (
       <ItemsCarousel
@@ -39,29 +46,30 @@ class MultiItemsCarousel extends React.Component {
         enablePlaceholder
         numberOfPlaceholderItems={20}
         minimumPlaceholderTime={1000}
-        placeholderItem={<div style={{ height: 87, background: '#900',align:'center' }}>Placeholder</div>}
-
+        placeholderItem={
+          <div style={{ height: 87, background: "#900", align: "center" }}>
+            Placeholder
+          </div>
+        }
         // Carousel configurations
         numberOfCards={6}
         gutter={12}
         showSlither={true}
         firstAndLastGutter={true}
         freeScrolling={false}
-
         // Active item configurations
         requestToChangeActive={this.changeActiveItem}
         activeItemIndex={activeItemIndex}
-        activePosition={'center'}
-
+        activePosition={"center"}
         chevronWidth={25}
-        rightChevron={'>'}
-        leftChevron={'<'}
+        rightChevron={">"}
+        leftChevron={"<"}
         outsideChevron={false}
       >
         {children}
       </ItemsCarousel>
-    );  
+    );
   }
-} 
+}
 
 export default MultiItemsCarousel;
